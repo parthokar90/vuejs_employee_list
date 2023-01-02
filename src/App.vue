07773d0d88fafd0fb26a818f9c7f9b88522e8400
parthokar90 <parthokar90@gmail.com>
2023-01-02 16:14:01 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+     <EmployeeList :AllEmployeList="AllEmployeList"/>
+</template>
+
+<script>
+import EmployeeList from './components/EmployeeList.vue'
+
+export default {
+  components: {
+    EmployeeList
+  },
+  data(){
+    return {
+      AllEmployeList:[
+      {
+        id:'001',
+        name:'Tanveer Vaia',
+        status:'Active',
+      },
+      {
+        id:'002',
+        name:'Morsalin Vaia',
+        status:'Active',
+      },
+      {
+        id:'003',
+        name:'Partho',
+        status:'Inactive',
+      }, 
+      {
+        id:'004',
+        name:'Khadiza Apu',
+        status:'Inactive',
+      }, 
+      ],
+    }
+  }
+}
+</script>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+
+     <div class="col-md-4">
+       <h4>All Employee List</h4>
+       <table class="table table-bordered">
+         <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Status</td>
+         </tr>
+
+         <tr v-for="item in AllEmployeList" :key="item.id">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.status}}</td>
+         </tr>
+       </table>
+     </div>
+
+     <div class="col-md-4">
+       <h4>Active Employee List</h4>
+         <table class="table table-bordered">
+         <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Status</td>
+         </tr>
+
+          <tr v-for="item in activeEmployee" :key="item.id">
+            
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.status}}</td>
+         </tr>
+       </table>
+     </div>
+
+     <div class="col-md-4">
+         <h4>Inactive Employee List</h4>
+         <table class="table table-bordered">
+         <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Status</td>
+         </tr>
+
+          <tr v-for="item in inactiveEmployee" :key="item.id">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.status}}</td>
+         </tr>
+       </table>
+     </div>
+
+  </div>
+</template>
+
+<style>
+table tr td{
+  border:1px solid #000000;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    AllEmployeList:[],
+  },
+  computed: {
+  activeEmployee: function () {
+    return this.AllEmployeList.filter(i => i.status === 'Active')
+  },
+  inactiveEmployee: function () {
+   return this.AllEmployeList.filter(i => i.status === 'Inactive')
+  }
+}
+}
+</script>
+
+ 
